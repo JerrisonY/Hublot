e53999e837b11318d0b6ec081ee6b1b0360e1aad
Jerrison Yip <jerryyip24@gmail.com>
2024-03-14 01:14:44 -0400

    useEffect for navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,30 @@
-import './NavBar.scss'
 import { NavLink } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import './NavBar.scss'
 
 function NavBar() {
-    // const [navScroll, setNavScroll] = useState(false);
-    
+    const [prevScrollPos, setPrevScrollPos] = useState(window.scrollY);
+    const [visible, setVisible] = useState(true);
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollPos = window.scrollY;
+            const visible = prevScrollPos > currentScrollPos;
+
+            setPrevScrollPos(currentScrollPos);
+            setVisible(visible);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [prevScrollPos]);
+    
   return (
-    <nav className="nav-container">
+    <nav 
+        className={`nav-container ${visible ? 'visible' : 'hidden'} 
+        ${(window.scrollY > 100) ? 'default-nav' : ''}`}
+    >
         <NavLink to='/'>
             <img src="public/hublot.svg" alt="" />
         </NavLink>
