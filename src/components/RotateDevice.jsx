a8503c687a751ca24cdf0b41282979aff3e38dbc
Jerrison Yip <jerryyip24@gmail.com>
2024-03-14 18:07:26 -0400

    added rotate device component

---
+++ src/components/RotateDevice.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './RotateDevice.css'
+
+function RotateDevice() {
+  return (
+    <div id='landscape-msg'>
+        <div className="phone"></div>
+        <div className="message">
+          Please rotate your device!
+        </div>
+      </div>
+  )
+}
+
+export default RotateDevice
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import './Home.scss'
+import RotateDevice from '../../components/RotateDevice.jsx'
 import HomeVideo from '../../components/HomeVideo.jsx'
 
 function LandingPage() {
 
   return (
     <>
+      <RotateDevice />
       <HomeVideo />
       <section className='home-feature'>
 
