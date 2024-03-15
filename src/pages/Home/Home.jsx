79c6213a065d848b3c9a4f8fa6c72c99442fd521
Jerrison Yip <jerryyip24@gmail.com>
2024-03-14 23:58:06 -0400

    added home feature and styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,10 @@
 import './Home.scss'
+import { motion } from 'framer-motion'
 import RotateDevice from '../../components/RotateDevice.jsx'
 import HomeVideo from '../../components/HomeVideo.jsx'
+
+import featureImage from '../../assets/img/home/feature.png'
+import hublotSpin from '../../assets/img/home/hublotspin.mp4'
 
 function LandingPage() {
 
@@ -8,8 +12,23 @@ function LandingPage() {
     <>
       <RotateDevice />
       <HomeVideo />
-      <section className='home-feature'>
 
+      <section className='home-feature'>
+        <div className='hublot-spin-vid-c'>
+          <video autoPlay loop muted playsInline >
+            <source src={hublotSpin} type='video/mp4' />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <motion.div
+          className='hublot-spin-text'
+        >
+          <span>takashi murakami</span>
+          <h1>color<br/>stone</h1>
+          <span className='hublot-price'>45mm cad 168,000</span>
+          <br />
+          <button>shop now</button>
+        </motion.div>
       </section>
     </>
   )
