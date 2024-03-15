bda4ffdbeeb2604c10e9052e98cbbdabc7508763
Jerrison Yip <jerryyip24@gmail.com>
2024-03-15 16:10:48 -0400

    home feature media query

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,18 +14,25 @@ function LandingPage() {
       <HomeVideo />
 
       <section className='home-feature'>
-        <div className='hublot-spin-vid-c'>
+        <motion.div 
+          className='hublot-spin-vid-c'
+          initial={{ opacity: 0, x: -500 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 2, delay: 0 }} 
+          viewport={{ once: true, amount: 0 }}
+        >
           <video autoPlay loop muted playsInline >
             <source src={hublotSpin} type='video/mp4' />
             Your browser does not support the video tag.
           </video>
-        </div>
+        </motion.div>
         <motion.div
           className='hublot-spin-text'
         >
           <span>takashi murakami</span>
           <h1>color<br/>stone</h1>
-          <span className='hublot-price'>45mm cad 168,000</span>
+          <span className='hublot-price'>45mm &nbsp;&nbsp;cad 168,000</span>
           <br />
           <button>shop now</button>
         </motion.div>
