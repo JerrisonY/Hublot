75bac72afdd305de1f79bf74e4b0eeea2a803108
Jerrison Yip <jerryyip24@gmail.com>
2024-04-18 21:43:08 -0400

    takashi feature images height changed

--- src/components/HomeWatches.jsx
+++ src/components/HomeWatches.jsx
@@ -3,11 +3,22 @@ import { motion } from 'framer-motion'
 
 function HomeWatches() {
   return (
-    <section className="home-watches-c">
-        <div className='h-watches-first'>
-            
-        </div>
-    </section>
+    <>
+        <section>
+            <motion.div
+                className=''
+                initial={{ opacity: 0, x: 100, y: 50 }}
+                whileInView={{ opacity: 1, x: 0, y: 0 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.6, delay: 0.1 }} 
+                viewport={{ once: true, amount: 0 }}
+            >
+                <span></span>
+            </motion.div>
+        </section>
+    
+    
+    </>
   )
 }
 
