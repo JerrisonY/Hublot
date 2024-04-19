06fa44323f0e530e5ac978b95fdf0a4578952ed0
Jerrison Yip <jerryyip24@gmail.com>
2024-04-19 18:23:52 -0400

    js for hidden/visible backtotop

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
 import './BackToTop.scss'
+import { useState, useEffect } from 'react';
 
 function BackToTop() {
+    const [visible, setVisible] = useState(false);
+    const [windowInnerWidth, setWindowInnerWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollPos = window.scrollY;
+            const visible = 1300 < currentScrollPos;
+
+            if (window.scrollY >= 200)
+                setVisible(visible);
+        };
+
+        const handleResize = () => {
+            setWindowInnerWidth(window.innerWidth);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener('resize', handleResize);
+        };
+
+    }, [windowInnerWidth]);
+
   return (
-    <div className='back-top-c'>
+    <div 
+        className={`back-top-c ${visible ? 'visible' : 'hidden'}`}
+    >
         <p>back to top</p>
         <i class="fa-solid fa-chevron-up"></i>
     </div>
